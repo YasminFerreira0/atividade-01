3284dc0b081e30cf776599a5362bd0a87b68fe20
Yasmin Ferreira <yasmincferreira8@gmail.com>
2023-03-01 15:22:50 -0400

    up

--- questao-06.c
+++ questao-06.c
@@ -4,12 +4,12 @@ void main()
     float salario, r1, r2, r3;
     printf("\nInforme o salario do funcionario: ");
     scanf("%f", &salario);
-    r1 = (salario * 0.07);
-    r2 = (r1 * 0.06);
-    r3 = (r2 * 0.05);
+    r1 = (salario * 0.07) + salario;
+    r2 = (r1 * 0.06) + r1;
+    r3 = (r2 * 0.05) + r2;
 
-    printf("\nOsalrio do primeiro reajuste e: %f", r1);
-    printf("\nOsalrio do segundo reajuste e: %f", r2);
-    printf("\nOsalrio do terceiro reajuste e: %f", r3);
+    printf("\nOsalrio do primeiro reajuste e: %.2f", r1);
+    printf("\nOsalrio do segundo reajuste e: %.2f", r2);
+    printf("\nOsalrio do terceiro reajuste e: %.2f", r3);
 
 }
